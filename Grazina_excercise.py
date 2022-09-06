ed12f5a4d600832e115ac82870325461aa02f83c
Grazina-S <112970276+Grazina-S@users.noreply.github.com>
2022-09-06 20:52:19 +0300

    Add files via upload

---
+++ Grazina_excercise.py
@@ -0,0 +1,10 @@
+# * print your name 5 times
+# * print Bananananana using "Ba" once and "na" 5 times
+# * print the number of seconds in a year calculated from months, days, minutes, seconds
+# * how much a googol is that is 10 to the power of the 100th
+# * how much will your 1000 Euro investment be worth after 12 years of 6% yearly interest?
+print("Grazina " * 5)
+print("Ba" + "na" * 5)
+print(12 * 30 * 1440 * 60)
+print(10 ** 100)
+print(1000 * 1.06 ** 12)
