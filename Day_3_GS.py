09187ceff3c787bfb5f3928cf10d06671e695ada
Grazina-S <112970276+Grazina-S@users.noreply.github.com>
2022-09-08 20:52:29 +0300

    Add files via upload

---
+++ Day_3_GS.py
@@ -0,0 +1,18 @@
+# Write a program that asks for and saves a username
+# Ask a question about the user's age, using the username in the question.
+# Shows in how many years the user will be 100 years old smile
+# BONUS: 
+# then you will need two additional lines:
+# import datetime # let's talk about imports separately
+# currentYear = datetime.datetime.now (). yearLet the program also show the year when the user will be 100 years old.
+# It could use a variable with the current year.
+# It would be even better to get the current year automatically
+username = input("What is your username?")
+age = input(f"How old are you {username} ?")
+age = int(age)
+age_100 = 100 - age
+print(f"So you will be 100 in {age_100} years 😀 ")
+import datetime
+current_year = datetime.datetime.now().year
+age_100_year = current_year + age
+print(f"So you will be 100 years old in year {age_100_year}")
